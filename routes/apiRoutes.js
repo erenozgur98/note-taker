6676c7cde14452d6dd3ad4e0c4001bacd1347a90
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-03-12 21:20:06 -0500

    added code to apiroutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,10 @@
-const db = require('./db/db.json');
+const fs = require('fs');
 
 
 module.exports = (app) => {
     app.get('./api/notes', (req, res) => res.json(db));
 
+    app.post('./api/notes', (req, res) => {
+        
+    })
 }
