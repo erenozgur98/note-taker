40cda4a4bcfdb59eff616ee30471da992c984e97
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-03-15 18:33:07 -0400

    added the funcionality of delete button

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,23 @@
+const fs = require('fs');
 const note = require('../db/db.json');
+const uuid = require('uuid');
 
 module.exports = (app) => {
-    app.get('./api/notes', (req, res) => res.json(note));
+    app.get('/api/notes', (req, res) => res.json(note));
 
-    app.post('./api/notes', (req, res) => {
+    app.post('/api/notes', (req, res) => {
+       const newId = uuid;
+       req.body['id'] = newId;
        note.push(req.body);
        res.json(true);
+       fs.writeFileSync('./db/db.json', JSON.stringify(note));
     });
-}
+
+    app.delete('/api/notes/:id', (req, res) => {
+        const noteIndex = note.findIndex(id => id === req.params.id);
+        note.splice(noteIndex, 1);
+        fs.writeFileSync('./db/db.json', JSON.stringify(note));
+        res.end();
+    });
+};
+
