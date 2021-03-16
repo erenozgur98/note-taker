71932e10e4f5fc1a8eeaa99dbbaf589d7f39c221
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-03-15 20:04:24 -0400

    done last touch to the code to make sure it works properly

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.use(express.static('public'));
 require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
 
-app.listen(PORT, () => { 
+app.listen(PORT, () => {
     console.log(`Listening on PORT: ${PORT}`);
 });
